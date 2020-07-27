b9aec919ddc041f2a6240975e17f0189ac9574ba
Thalyson <Thalysonr.castro@hotmail.com>
2020-07-27 19:23:59 -0400

    config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: "AlgoSite", // Title for your website.
   tagline: "Um site com conceitos básicos de programação",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
-  baseUrl: "/", // Base URL for your project */
+  url: "https://ThalysonR.github.io", // Your website URL
+  baseUrl: "/AlgoSite/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "test-site",
-  organizationName: "facebook",
+  projectName: "AlgoSite",
+  organizationName: "ThalysonR",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
